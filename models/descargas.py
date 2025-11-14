a7bcd72207d5edb65fe537b84427fb91240d0186
GakeBruh <kevingabrielfloresflores@gmail.com>
2025-11-14 02:07:53 -0600

    0.0.1

---
+++ models/descargas.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, Field, field_validator
+from typing import Optional
+from datetime import date, datetime
+import re
+
+class Offline(BaseModel):
+
+    id_carpeta_offline: Optional[int] = Field(
+        description = "ID de la carpeta descargada"
+    )
+    
+    id_archivo_offline: Optional[int] = Field(
+        description = "ID del archivo descargado"
+    )
+
+    id_usuario: int = Field(
+        description = "ID del usuario que descargo"
+    )
+
+    fecha_sincronizacion: datetime = Field(
+        description = "Fecha de descarga inicial"
+    )
+    
+    fecha_de_actualizacion: datetime = Field(
+        description = "Fecha de actualizacion de la descarga"
+    )
+    
+    local_path: str = Field(
+        description = "Ruta de la descarga"
+    )
