f6574dc4ed65305523d213ca202cd40d49d6f040
GakeBruh <kevingabrielfloresflores@gmail.com>
2025-11-26 21:19:14 -0600

    1.0.0

--- models/comentarios.py
+++ models/comentarios.py
@@ -1,22 +1,31 @@
 from pydantic import BaseModel, Field
 from typing import Optional
-from datetime import datetime
+from datetime import date
 
 class Comentario(BaseModel):
     id: Optional[int] = Field(
         default=None,
         description="ID del comentario"
     )
+
     id_usuario: int = Field(
         description="ID del usuario que hace el comentario"
     )
+
     id_archivo: int = Field(
         description="ID del archivo al que se hace el comentario"
     )
+
     texto: str = Field(
         description="Texto del comentario"
     )
-    fecha: Optional[datetime] = Field(
+
+    fecha: Optional[date] = Field(
         default=None,
         description="Fecha de cuando se hizo el comentario"
+    )
+
+class ComentarioUpdate(BaseModel):
+    texto: str = Field(
+        description="Texto del comentario"
     )
