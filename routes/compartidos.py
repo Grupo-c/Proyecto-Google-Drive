30b6d65e6f08114591e301e2eded7783c9759d3d
Jahilin <cantarerojahilin@gmail.com>
2025-11-26 21:43:17 -0600

    Update compartidos.py

--- routes/compartidos.py
+++ routes/compartidos.py
@@ -23,7 +23,7 @@ async def create_new_compartido(compartido_data: Compartido):
     return await create_compartido(compartido_data)
 
 @router.put("/{id}", tags=["Compartidos"], status_code=status.HTTP_200_OK)
-async def update_compartido_info(id: int, compartido_data: CompartidosUpdate):
+async def update_compartido_info(id: int, compartido_data: CompartidoUpdate):
     return await update_compartido(id, compartido_data)
 
 @router.delete("/{id}", tags=["Compartidos"], status_code=status.HTTP_200_OK)
